56d450b5f2fb2d2fbb99e1aabd1e392e36ef01c8
Landon <landon.creel@protonmail.com>
2026-01-12 19:09:31 -0800

    Adding Gin Server

--- main.go
+++ main.go
@@ -1,9 +1,22 @@
 package main
 
 import (
-	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	fmt.Println("Hello, Nobots!")
+	// Create a Gin router with default middleware (logger and recovery)
+	r := gin.Default()
+
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+
+	// Start Server on Port 8080 (default)
+	// Server will listen on 0.0.0.0.8080 (localhost:8080 on Windows)
+	r.Run()
 }
